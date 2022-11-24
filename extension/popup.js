60ae96126e3c69147d3368470ea7cfcc42cb3dc4
Gopal Meena <meenagopal24@gmail.com>
2022-11-24 23:17:15 +0530

    final youtube version

--- extension/popup.js
+++ extension/popup.js
@@ -1,26 +1,47 @@
 window.onload = function () {
-	var dButton = document.getElementById('fillButton');
-	dButton.onclick = function () {
-		console.log("button  clicked");
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			var url = tabs[0].url;
-			var message = {
-				'url': url,
-			};
+  var dButton = document.getElementById("fillButton");
+  dButton.onclick = function () {
+    console.log("button  clicked");
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var url = tabs[0].url;
+      var message = {
+        url: url,
+      };
+      window.open(`http://localhost:5000/?url=${url}`, "_blank");
+      //   const req = new XMLHttpRequest();
+      //   const baseUrl = "http://localhost:5000/upload";
 
-			const req = new XMLHttpRequest();
-			const baseUrl = "http://localhost:5000/upload";
+      //   req.open("POST", baseUrl, true);
+      //   req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      //   req.send(JSON.stringify(message));
+      //   req.onload  = function() {
+      //   	var jsonResponse = req.response;
+      //   	chrome.runtime.sendMessage(jsonResponse);
+      //    };
+    });
+  };
 
-			req.open("POST", baseUrl, true);
-			req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-			req.send(JSON.stringify(message));
-			req.onload  = function() {
-				var jsonResponse = req.response;
-				chrome.runtime.sendMessage(jsonResponse);
-			 };
+  chrome.tabs.getSelected(null, function (tab) {
+    if (tab && tab.status == "complete") {
+      chrome.tabs.executeScript(tab.id, { file: "content.js" }, function () {
+        popup();
+        chrome.tabs.executeScript(
+          tab.id,
+          { code: "var right_clicked = false;" },
+          function () {
+            chrome.tabs.executeScript(tab.id, { file: "jq.js" });
+          }
+        );
+      });
+    }
+  });
+};
 
-
-
-		});
-	};
+function popup() {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, { message: "start" }, (res) => {
+      document.getElementById("file_name").innerHTML = res.title;
+    });
+  });
 }
--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
-const express = require('express');
+const express = require("express");
+const ytdl = require("ytdl-core");
 // const youtube = require('ytdl-core');
 const app = express();
 app.use(express.json());
-app.use(
-    express.urlencoded({extended: true})
-)
-
-app.listen('5000', function () {
-    console.log("Server is started successfully");
+app.use(express.urlencoded({ extended: true }));
+const dateObject = new Date();
+const seconds = dateObject.getSeconds();
+app.listen("5000", function () {
+  console.log("Server is started successfully");
 });
 
-app.get('/downloads', (req, res) => {
-    
+app.get("/", (req, res) => {
+  let videoURL = req.query.url;
+  console.log(videoURL);
+  res.writeHead(200, {
+    "Content-Disposition": `attachment; filename= ${seconds}+video.mp4`,
+  });
+  ytdl(videoURL, {
+    format: "mp4",
+  }).pipe(res);
 });
 
-app.post('/upload', (req, res) => {
-    console.log(JSON.stringify(req.body.url))
-    res.send({"url" : req.body.url});
-});
+// app.post("/upload", (req, res) => {
+//   console.log(JSON.stringify(req.body.url));
+//   res.writeHead(200, {
+//     "Content-Disposition": 'attachment; filename="video.mp4"',
+//   });
+//   ytdl(req.body.url, {
+//     format: "mp4",
+//   }).pipe(res);
+//   // res.send({"url" : req.body.url});
+// });
